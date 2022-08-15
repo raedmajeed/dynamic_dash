652644246089a0d2526b0ff6f4fad80e0dcf3bbb
Akshay Mohan <akshaymohan99@gmail.com>
2022-08-15 20:06:15 +0530

    initial_setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className='rounded bg-slate-200'>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
